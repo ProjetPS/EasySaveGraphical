ad97bd1b593737b8797c8b8d453fa38017e5dfcc
Arthur Joye <arthur.joye@viacesi.fr>
2022-12-05 14:41:20 +0100

    Update Settings form

--- EasySaveGraphic/settings.xaml.cs
+++ EasySaveGraphic/settings.xaml.cs
@@ -23,9 +23,28 @@ namespace EasySaveGraphic
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Button_Software(object sender, RoutedEventArgs e)
         {
+            // Configure open file dialog box
+            var dialog = new Microsoft.Win32.OpenFileDialog();
+            dialog.FileName = "Document"; // Default file name
+            dialog.DefaultExt = ".txt"; // Default file extension
 
+            // Show open file dialog box
+            bool? result = dialog.ShowDialog();
+
+            // Process open file dialog box results
+            if (result == true)
+            {
+                // Open document
+                string filename = dialog.SafeFileName;
+                ShowDialog.Text = filename;
+            }
+        }
+
+        private void Button_Save(object sender, RoutedEventArgs e)
+        {
+            
         }
     }
 }
