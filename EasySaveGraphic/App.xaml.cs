6ecf9f52e826135de08669a74fb2ac38b268717f
Maxence A <maxence.arcelin@viacesi.fr>
2022-12-09 10:47:16 +0100

    EasySave is now mono instance (App.xaml.cs)

--- EasySaveGraphic/App.xaml.cs
+++ EasySaveGraphic/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Diagnostics;
 
 namespace EasySaveGraphic
 {
@@ -13,6 +14,14 @@ namespace EasySaveGraphic
     /// </summary>
     public partial class App : Application
     {
-
+        private App()
+        {
+            // Check if EasySave process is already executed
+            Process[] appByName = Process.GetProcessesByName("EasySaveGraphic");
+            if(appByName.Length > 1) // if there are more than 1 process (including this one)
+            {
+                System.Windows.Application.Current.Shutdown(); // shutdown before anything
+            }
+        }
     }
 }
